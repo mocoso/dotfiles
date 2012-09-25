d85fe2ad823e3ba152c9d3a34f7dbdade6448afe
Joel Chippindale <joel@joelchippindale.com>
2012-09-25 08:38:27 +0100

    Basic zsh completion configuration

---
+++ home/.zsh.d/completion.zsh
@@ -0,0 +1,12 @@
+# initialize autocomplete here, otherwise functions won't be loaded
+autoload -U compinit
+compinit
+
+zstyle ':completion:*:*:*:*:*' menu select
+
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
+
+# Use caching so that commands like apt and dpkg complete are useable
+zstyle ':completion::complete:*' use-cache 1
+zstyle ':completion::complete:*' cache-path $ZSH/cache/
--- home/.zshrc
+++ home/.zshrc
@@ -1,10 +1,6 @@
 # shortcut to this dotfiles path is $ZSH
 export ZSH=$HOME/.zsh.d
 
-# initialize autocomplete here, otherwise functions won't be loaded
-autoload -U compinit
-compinit
-
 # source every .zsh file in this rep
 for config_file ($ZSH/*.zsh) source $config_file
 
