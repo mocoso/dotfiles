8b5972f062594875b0823ab46fe131fcf17c79bf
Joel Chippindale <joel@joelchippindale.com>
2013-11-02 21:28:46 +0000

    Install apps last

--- osx_bootstrap.sh
+++ osx_bootstrap.sh
@@ -41,11 +41,6 @@ ensure_brew_installed 'mercurial' # for vim
 ensure_brew_installed 'vim'
 ensure_brew_installed 'ssh-copy-id'
 
-check_app_installed "Dropbox" "open https://www.dropbox.com; exit_with_instructions 'Install Dropbox from https://www.dropbox.com'"
-check_app_installed "GitX" "open http://gitx.frim.nl; exit_with_instructions 'Install GitX from http://gitx.frim.nl'"
-check_app_installed "iTerm" "open http://www.iterm2.com/#/section/home; exit_with_instructions 'Install iTerm from http://www.iterm2.com/#/section/home'"
-check_app_installed "Sidestep" "open http://chetansurpur.com/projects/sidestep/; exit_with_instructions 'Install Sidestep from http://chetansurpur.com/projects/sidestep/'"
-
 # terminal
 echo " ---> Check 256 colours are available in the terminal"
 case "$TERM" in
@@ -166,5 +161,10 @@ defaults write com.apple.dashboard mcx-disabled -boolean YES
 
 echo " ---> Symlink dotfiles"
 ./symlink.sh > /dev/null
+
+check_app_installed "Dropbox" "open https://www.dropbox.com; exit_with_instructions 'Install Dropbox from https://www.dropbox.com'"
+check_app_installed "GitX" "open http://gitx.frim.nl; exit_with_instructions 'Install GitX from http://gitx.frim.nl'"
+check_app_installed "iTerm" "open http://www.iterm2.com/#/section/home; exit_with_instructions 'Install iTerm from http://www.iterm2.com/#/section/home'"
+check_app_installed "Sidestep" "open http://chetansurpur.com/projects/sidestep/; exit_with_instructions 'Install Sidestep from http://chetansurpur.com/projects/sidestep/'"
 
 printf "\nReady to go\n"
