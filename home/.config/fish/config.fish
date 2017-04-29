e96b90b47a33f9148f26feb4276da4654d5bec96
Joel Chippindale <joel@joelchippindale.com>
2017-04-29 21:23:21 +0100

    Switch to using abbr instead of aliases in fish
    To see what it is like

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,7 +2,7 @@ set -g fish_color_cwd white
 set -g fish_color_uneditable_cwd white
 
 # bundler
-alias be="bundle exec"
+abbr be "bundle exec"
 
 # chruby
 source /usr/local/share/chruby/chruby.fish
@@ -15,21 +15,21 @@ alias gst="git status -sb"
 alias gup="git fetch ;and git rebase"
 alias gpu="git push"
 alias gc="git commit -v"
-alias gcm="git checkout master"
-alias gco="git checkout"
-alias gbr="git branch"
+abbr gcm "git checkout master"
+abbr gco "git checkout"
+abbr gbr "git branch"
 alias gcount="git shortlog -sn"
-alias gcp="git cherry-pick"
+abbr gcp "git cherry-pick"
 alias glg="git log --stat --max-count=10"
 alias gg="git log --oneline --abbrev-commit --graph --decorate"
 alias ggg="git log --oneline --abbrev-commit --all --graph --decorate"
-alias grb="git rebase"
+abbr grb "git rebase"
 alias gss="git status -s"
-alias gad="git add"
-alias gmg="git merge"
-alias gd="git diff"
-alias grh="git reset HEAD"
-alias grhh="git reset HEAD --hard"
+abbr gad "git add"
+abbr gmg "git merge"
+abbr gd "git diff"
+abbr grh "git reset HEAD"
+abbr grhh "git reset HEAD --hard"
 
 # homebrew
 # prioritise binaries installed by homebrew
