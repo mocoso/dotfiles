a2a00e0740f78d8f4dd63cb4373292b8f889251a
Joel Chippindale <joel@joelchippindale.com>
2017-04-29 07:13:24 +0100

    Add git aliases to fish shell

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -4,3 +4,26 @@ set -g fish_color_uneditable_cwd white
 # chruby
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
+
+# git
+set -U GIT_EDITOR "vim -f"
+
+alias gst="git status -sb"
+alias gup="git fetch ;and git rebase"
+alias gpu="git push"
+alias gc="git commit -v"
+alias gcm="git checkout master"
+alias gco="git checkout"
+alias gbr="git branch"
+alias gcount="git shortlog -sn"
+alias gcp="git cherry-pick"
+alias glg="git log --stat --max-count=10"
+alias gg="git log --oneline --abbrev-commit --graph --decorate"
+alias ggg="git log --oneline --abbrev-commit --all --graph --decorate"
+alias grb="git rebase"
+alias gss="git status -s"
+alias gad="git add"
+alias gmg="git merge"
+alias gd="git diff"
+alias grh="git reset HEAD"
+alias grhh="git reset HEAD --hard"
