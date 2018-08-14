7c8d9477d5b5c57269231e96fdc0cd026c6301fd
Joel Chippindale <joel@joelchippindale.com>
2018-08-14 23:23:45 +0100

    Remove iTerm
    Because Terminal is good enough

--- osx_bootstrap.sh
+++ osx_bootstrap.sh
@@ -170,7 +170,6 @@ echo " ---> Symlink dotfiles"
 ./symlink.sh > /dev/null
 
 check_app_installed "Dropbox" "open https://www.dropbox.com; exit_with_instructions 'Install Dropbox from https://www.dropbox.com'"
-check_app_installed "iTerm" "open http://www.iterm2.com/#/section/home; exit_with_instructions 'Install iTerm from http://www.iterm2.com/#/section/home'"
 check_app_installed "Sidestep" "open http://chetansurpur.com/projects/sidestep/; exit_with_instructions 'Install Sidestep from http://chetansurpur.com/projects/sidestep/'"
 
 echo " ---> Set up completions for fish"
